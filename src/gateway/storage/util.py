983514b328941a4df49d5e89eb2a704793a66e8e
DeanGarofalo <deanrgarofalo@gmail.com>
2023-12-26 19:43:45 -0500

    Add gateway component and k8s infra

---
+++ src/gateway/storage/util.py
@@ -0,0 +1,23 @@
+import pika, json
+
+def upload(f, fs, channel, access):
+    try:
+        fid = fs.put(f)
+    except Exception as err:
+        return "Internal Server Error", 500
+    
+    message = {
+        "video_fid": str(fid),
+        "mp4_fid": None,
+        "username": access["username"],
+    }
+    try:
+        channel.basic_publish(
+            exchange="", 
+            routing_key="video",
+            body=json.dumps(message),
+            properties=pika.BasicProperties(deliver_mode=pika.spec.PERSISTENT_DELIVERY_MODE)
+        )
+    except:
+        fs.delete(fid)
+        return "Internal Server Error", 500
